2492a5be82aa6c246bb343ccaf83133c8c35d8cb
Bilalbink1 <bilal@stacksync.com>
2025-07-09 00:49:32 +0200

    Credentials component added

--- frontend/src/components/CredentialCard/CredentialCard.tsx
+++ frontend/src/components/CredentialCard/CredentialCard.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import styles from "./CredentialCard.module.css";
 
-const CredentialCard = () => {
-    return <div>Credentials</div>;
+interface Props {
+    name: string;
+    description: string;
+}
+
+const CredentialCard = ({ name, description }: Props) => {
+    return (
+        <Card
+            className={styles["credential-card-container"]}
+            bg="light"
+            onClick={() => console.log("test")}
+        >
+            <Card.Body className={styles["credential-card-body"]}>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{description}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
 };
 
 export default CredentialCard;
--- frontend/src/pages/Credentials/Credentials.tsx
+++ frontend/src/pages/Credentials/Credentials.tsx
@@ -1,9 +1,40 @@
 import PageLayout from "../../components/PageLayout/PageLayout";
+import CredentialCard from "../../components/CredentialCard/CredentialCard";
+
+const sampleData = [
+    {
+        name: "Gym Membership",
+        descrption:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+    },
+    {
+        name: "Gym Membership",
+        descrption:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+    },
+    {
+        name: "Gym Membership",
+        descrption:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+    },
+    {
+        name: "Gym Membership",
+        descrption:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+    },
+];
 
 const Credentials = () => {
     return (
         <PageLayout title="Your Credentials">
-            <div>test</div>
+            <div>
+                {sampleData.map((data) => (
+                    <CredentialCard
+                        name={data.name}
+                        description={data.descrption}
+                    />
+                ))}
+            </div>
         </PageLayout>
     );
 };
